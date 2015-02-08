045fd5108bc0dfa4ebf8100c601ba0faf2b9e4cd
Rob Landry <rob.a.landry@gmail.com>
2015-02-08 15:27:48 -0500

    Version 1.0

--- Smart Lights.app.groovy
+++ Smart Lights.app.groovy
@@ -1,6 +1,8 @@
 /**
  *  Smart Lights
  *
+ *  Version: 1.0
+ *
  *  Copyright 2015 Rob Landry
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
