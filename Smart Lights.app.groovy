6a0c2d68aafd99618cecb35d7bce4971a027b5cf
Rob Landry <rob.a.landry@gmail.com>
2015-02-14 10:23:48 -0500

    Testing multiple light states

--- Smart Lights.app.groovy
+++ Smart Lights.app.groovy
@@ -1,7 +1,7 @@
 /**
  *  Smart Lights
  *
- *  Version: 1.2
+ *  Version: 1.2-multi
  *
  *  Copyright 2015 Rob Landry
  *
@@ -26,15 +26,20 @@ definition(
 
 
 preferences {
+	// the info section
 	section("Info") {
 		paragraph "Author:  Rob Landry"
-		paragraph "Version: 1.2"
-		paragraph "Date:    2/13/2015"
+		paragraph "Version: 1.2-multi"
+		paragraph "Date:    2/14/2015"
 	}
+
+	// the devices section
 	section("Devices") {
 		input "motion", "capability.motionSensor", title: "Motion Sensor", multiple: false
 		input "lights", "capability.switch", title: "Lights to turn on", multiple: true
 	}
+
+	// the preferences section
 	section("Preferences") {
 		paragraph "Motion sensor delay..."
 		input "motionEnabled", "bool", title: "Enable/Disable Motion Control.", required: true, defaultValue: true
@@ -56,9 +61,28 @@ def updated() {
 def initialize() {
 	subscribe(motion, "motion", motionHandler)
 	subscribe(lights, "switch", switchHandler)
-	state.lights = (lights.currentValue("switch")[0] == "on") ? true : false // only looks at first light
-	state.motionCommand = false
-    state.motionStopTime = null
+
+	// Establish Defaults for the lights
+	state.thelights = [:]
+	lights.each {
+		state.thelights[it.id] = 
+			["currentValue" : [ "switch" : it.currentValue("switch") ]
+			,"initialValue" : [ "switch" : it.currentValue("switch") ]
+			,"motionCommand" : false ]
+	}
+	/*lights.each {
+		if (it.id == "0274a9fa-f287-4922-a10f-500fd28437f4") {
+			it.off()
+		}
+	}
+    
+	state.currentLights = lights.currentValue("switch")
+	state.lights = (lights.currentValue("switch")[0] == "on") ? true : false
+	state.motionCommand = false*/
+
+	// More defaults
+	state.motionProgramActive = false
+	state.motionStopTime = null
 	log.debug "initialize: State: ${state}"
 }
 
@@ -66,19 +90,41 @@ def motionHandler(evt) {
 	log.debug "Motion Enabled: $motionEnabled"
 	log.debug "Motion Handler - ${evt.name}: ${evt.value}, State: ${state}"
 
+	// if motion is not enabled, what are we doing here, get out!
 	if (!motionEnabled) {return}
 
+	// if motion event is active
 	if (evt.value == "active") {
 		log.info "Motion Detected."
-		if (!state.lights) {
-			state.motionCommand = true
-			lights?.on()
+
+		// if the light is off, set the motionCommand and turn it on
+		lights.each {
+			if (state.thelights[it.id].currentValue.switch == "off") {
+				state.thelights[it.id].motionCommand = "true"
+				it.on()
+			}
 		}
+	// if the motion event is inactive
 	} else if (evt.value == "inactive") {
 		log.info "Motion Ceased."
-		if (state.lights && state.motionCommand) {
+
+		// if the light is on, was initially off, and activated by motion,
+		// allow the program to turn off
+		lights.each {
+			if ((state.thelights[it.id].currentValue.switch == "on") && 
+				(state.thelights[it.id].initialValue.switch == "off") &&
+				(state.thelights[it.id].motionCommand)) {
+				state.motionProgramActive = true
+			}
+		}
+
+		// if the program is active
+		if (state.motionProgramActive) {
 			state.motionStopTime = now()
+
+			// if we are delaying the turnoff, then schedule it, otherwise do it!
 			if(delayMinutes) {
+
 				// This should replace any existing off schedule
 				unschedule("turnOffMotionAfterDelay")
 				runIn(delayMinutes*60, "turnOffMotionAfterDelay", [overwrite: false])
@@ -92,34 +138,56 @@ def motionHandler(evt) {
 def switchHandler(evt) {
 	log.debug "Switch Handler Start: ${evt.name}: ${evt.value}, State: ${state}"
 
-	if (delayMinutes) { 
-			unschedule ("turnOffMotionAfterDelay")
-			//log.debug "Unscheduled"
-	}
+	// if we were delaying the turnoff, unschedule it
+	if (delayMinutes) { unschedule ("turnOffMotionAfterDelay") }
 
+	// if we are turning off the light
 	if (evt.value == "off") {
 		log.info "Turning off."
-        state.motionCommand = false
-	} else if (evt.value == "on") {
-		if (state.motionCommand == false) {
-			log.info "Turning on using SWITCH."
-		} else {
-			log.info "Turning on using MOTION."
+
+		// update initial values and reset the motion command
+		lights.each {
+			state.thelights[it.id].initialValue.switch =  it.currentValue("switch")
+			state.thelights[it.id].motionCommand = false
+				]
 		}
+
+	// if we are turning it on, let us know
+	} else if (evt.value == "on") {
+		log.info "Turning on."
 	}
 
-	state.lights = (lights.currentValue("switch")[0] == "on") ? true : false
+	// reset the current value to actual state
+	lights.each {
+		state.thelights[it.id].currentValue.switch =  it.currentValue("switch")
+	}
+
+	/*state.currentLights = lights.currentValue("switch")
+	state.lights = (lights.currentValue("switch")[0] == "on") ? true : false*/
 	log.debug "Switch Handler End: ${evt.name}: ${evt.value}, State: ${state}"
 }
 
 def turnOffMotionAfterDelay() {
 	log.debug "turnOffMotionAfterDelay: State: ${state}"
 
-	if (state.motionStopTime && state.lights && motionEnabled) {
+	// if there is a motion allowed, we are in an active program, and a time was specified...
+	if (state.motionStopTime && state.motionProgramActive && motionEnabled) {
+
+		// get the time elapsed
 		def elapsed = now() - state.motionStopTime
+
+		// if we are at the time to turn off...
 		if (elapsed >= (delayMinutes ?: 0) * 60000L) {
-			state.motionCommand = true
-			lights.off()
+
+			// if the light is on, and was initially off,
+			// set the motion command to true, and turn off
+			lights.each {
+				if ((state.thelights[it.id].currentValue.switch == "on") && 
+					(state.thelights[it.id].initialValue.switch == "off")) {
+					state.thelights[it.id].motionCommand = "true"
+					it.off()
+				}
+			}
 		}
 	}
 }
